325896e2e5041764f026c149a46f3c38279c2364
Yuji Nakayama <nkymyj@gmail.com>
2015-03-22 21:04:08 +0900

    bundle gem rubygems-xcodeproj_generator

---
+++ lib/rubygems/xcodeproj_generator.rb
@@ -0,0 +1,7 @@
+require "rubygems/xcodeproj_generator/version"
+
+module Rubygems
+  module XcodeprojGenerator
+    # Your code goes here...
+  end
+end
---
+++ lib/rubygems/xcodeproj_generator/version.rb
@@ -0,0 +1,5 @@
+module Rubygems
+  module XcodeprojGenerator
+    VERSION = "0.1.0"
+  end
+end
